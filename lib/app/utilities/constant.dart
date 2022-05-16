80394a15186a14e6996dae62b20363e63aa2ed8a
imamnurhy <imamnurhy@gmail.com>
2022-05-16 20:12:24 +0700

    Change color background

--- lib/app/utilities/constant.dart
+++ lib/app/utilities/constant.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-Color primaryColor = const Color.fromARGB(255, 17, 16, 58);
-Color accentColor = const Color.fromARGB(255, 27, 28, 72);
+Color primaryColor = const Color.fromARGB(255, 139, 44, 148);
+Color accentColor = const Color.fromARGB(255, 94, 37, 99);
 Color selectedColor = const Color.fromARGB(255, 254, 207, 88);
 Color white = Colors.white;
 Color transparent = Colors.transparent;
